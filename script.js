37d1f01dc1b555aa27b6c29800cf45bca6d54a92
Sharon Chawanji <35290804+schawanji@users.noreply.github.com>
2022-08-04 16:06:22 +0200

    Update script.js
    Removed hard-coded variable and created a JS Class Character to to create player variables

--- script.js
+++ script.js
@@ -9,7 +9,44 @@ const images = {};
 images.player = new Image();
 images.player.src = 'sprites/character.png'
 
-const playerWidth = 103.0625
+// Array of rows of sprite sheet.
+
+const characterActions = [' up', 'top right', 'right', 'down right', 'down', 'jump'];
+const characters = [];
+
+class Character {
+    constructor() {
+        this.width = 103.0625;
+        this.height = 113.125;
+        this.frameX = 3;
+        this.frameY = 3;
+        this.x = 0;
+        this.y = 0;
+        this.speed = (Math.random() * 1.5) + 3.5;
+        this.action = 'right'
+    }
+    draw() {
+        // Call drawSprite() function to draw image on canvas.
+        drawSprite(images.player,
+            this.width * this.frameX,
+            this.height * this.frameY,
+            this.width, this.height,
+            this.x, this.y, this.width, this.height)
+        //animate sprites
+        if (this.frameX < 13) this.frameX++;
+        else this.frameX = 3;
+    }
+    update() {
+        if (this.action === 'right') {
+            if (this.x < canvas.width + this.width) this.x += this.speed;
+            else this.x = 0 - this.width
+        }
+    }
+}
+
+characters.push(new Character())
+
+/*const playerWidth = 103.0625
 const playerHeight = 113.125
 
 // Location of sprite is in the image
@@ -19,8 +56,7 @@ let playerFrameY = 3;
 // Location of sprite is in the canvas
 let playerX = 0;
 let playerY = 0;
-
-const playerSpeed = 6;
+const playerSpeed = 6;*/
 
 //Draw sprite on canvas from image. Funtion to draw takes in 9 parameters.
 
@@ -36,16 +72,17 @@ function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
 function animate() {
     // Cleans the whole canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    // Call drawSprite() function to draw image on canvas.
+    /*// Call drawSprite() function to draw image on canvas.
     drawSprite(images.player, playerWidth * playerFrameX, playerHeight * playerFrameY, playerWidth, playerHeight, playerX, playerY, playerWidth, playerHeight)
     //animate sprites
     if (playerFrameX < 13) playerFrameX++;
     else playerFrameX = 3;
-
     // move player
     if (playerX < canvas.width + playerWidth) playerX += playerSpeed;
-    else playerX = 0- playerWidth
+    else playerX = 0 - playerWidth*/
 
+    characters[0].draw();
+    characters[0].update();
 }
 
 // Animate at intervals
